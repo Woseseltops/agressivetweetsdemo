63a3e37f8d768fb406705f8ef56f419805524e38
Wessel Stoop <woseseltops@hotmail.com>
2014-11-05 17:27:49 +0100

    Added analyze response

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cherrypy
+import tweetlib
 
 template_folder = 'templates/';
+log_folder = 'log/';
+data_folder = 'tweets/';
 
 class agressive_tweets_demo(object):
 
@@ -9,7 +12,18 @@ class agressive_tweets_demo(object):
 
 	return open(template_folder+'enter_user.html').read();
 
-    index.exposed = True
+    index.exposed = True;
+
+    def analyze(self,user):
+        """Starts the import and shows the progress""";
+
+        tweets = tweetlib.get_all_tweets(user);
+        tweet_str = [str(tweet) for tweet in tweets];
+        open(data_folder+'user','w').write('\n'.join(tweet_str));
+
+        return open(template_folder+'analyze.html').read();
+
+    analyze.exposed = True;
 
     def log_file(self,user):
         """Returns the current logfile for the model creation of a user""";
