3042581589b86a3c282472cf6e281d2a6c1a8092
Wessel Stoop <woseseltops@hotmail.com>
2014-11-21 17:18:07 +0100

    Write results of agresiion analyzer to disk

--- agression_analyzer.py
+++ agression_analyzer.py
@@ -2,11 +2,18 @@ import random
 
 class Agression_analyzer():
 
-	def analyze(self,tweets):
+	def analyze(self,tweets,resultpath):
 		
 		results = [];
 		
+		#Get a score for all tweets
 		for tweet in tweets:
-			results.append((tweet,random.randrange(1,100)));
+			results.append((tweet.text,random.randrange(1,100)));
 
-		return results
+		results = sorted(results,key=lambda x: x[1],reverse=True);
+		print(results);
+
+		#Write the results away
+		results = [(txt,str(score)) for txt,score in results];
+		lines = ['\t'.join(line) for line in results];
+		open(resultpath,'w').write('\n'.join(lines));
