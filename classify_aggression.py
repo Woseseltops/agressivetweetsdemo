c99e678a6e502c43a8a7c7a66422457567c0677e
Wessel Stoop <wstoop@applejack.science.ru.nl>
2015-06-25 12:41:34 +0200

    Improvements to classifier by @fkunneman

--- classify_aggression.py
+++ classify_aggression.py
@@ -1,55 +1,75 @@
+# 2015.03.23 15:28:24 CET
+# Embedded file name: /www/aggressivetweets/live/repo/aggrestweets/classify_aggression.py
 from sklearn.externals import joblib
 import ucto
 import re
 import cPickle
 import codecs
+from collections import defaultdict
 
 class Classifier:
 
     def __init__(self):
-        clfmodel = open('model.joblib.pkl', 'rb')      
+        clfmodel = open('model.joblib.pkl', 'rb')
         self.clf = cPickle.load(clfmodel)
+        self.tokenizer = ucto.Tokenizer('/vol/customopt/uvt-ru/etc/ucto/tokconfig-nl-twitter')
         self.vocabulary = {}
-        vocabularyfile = codecs.open("vocabulary.txt","r","utf-8")
-        for i,line in enumerate(vocabularyfile.readlines()):
+        vocabularyfile = codecs.open('vocabulary.txt', 'r', 'utf-8')
+        for i, line in enumerate(vocabularyfile.readlines()):
             self.vocabulary[line.strip()] = i
+
         vocabularyfile.close()
         self.vocab_size = len(self.vocabulary.keys())
-        self.tokenizer = ucto.Tokenizer("/vol/customopt/uvt-ru/etc/ucto/tokconfig-nl-twitter")
+        self.idf = {}
+        idffile = codecs.open('log/test.txt', 'r', 'utf-8')
+        for line in idffile.readlines():
+            tokens = line.strip().split('\t')
+            self.idf[int(tokens[0])] = float(tokens[1])
 
-    #extract uni-, bi- and trigrams
-    def tweet_2_features(self,texts):
+        idffile.close()
+
+    def tweet_2_features(self, texts):
         vectors = []
         vectorframe = [float(0)] * self.vocab_size
         for text in texts:
-            self.tokenizer.process(unicode(text.decode('cp1252','ignore')))
-            tokens = [x.text for x in self.tokenizer]
-            for i,token in enumerate(tokens):
-                if token[0] == "@":
-                    tokens[i] = "USER"
-                if re.search(r"^http",token):
-                    tokens[i] == "URL"
-            ngrams = tokens + ["_".join(x) for x in zip(tokens, tokens[1:])] + ["_".join(x) for x in zip(tokens, tokens[1:], tokens[2:])]
-        #    print(ngrams)
+            self.tokenizer.process(unicode(text.decode('cp1252', 'ignore').lower()))
+            tokens = [ x.text for x in self.tokenizer ]
+            for i, token in enumerate(tokens):
+                if token[0] == '@':
+                    tokens[i] = 'USER'
+                if re.search('^http', token):
+                    tokens[i] == 'URL'
+
+            tokens_n = ['<s>'] + tokens + ['<s>']
+            ngrams = tokens + [ '_'.join(x) for x in zip(tokens_n, tokens_n[1:]) ] + [ '_'.join(x) for x in zip(tokens_n, tokens_n[1:], tokens_n[2:]) ]
+            print ngrams
             vector = vectorframe[:]
+            feature_freq = defaultdict(int)
             for feature in ngrams:
                 try:
-                    vector[self.vocabulary[feature]] = float(1)
+                    feature_index = self.vocabulary[feature]
+                    if re.search('kutwijf', feature):
+                        print (feature, feature_index)
+                    feature_freq[feature_index] += 1
                 except KeyError:
-         #           print("KeyError",feature)
                     continue
-          #  print(vector)
-            vectors.append((text,vector))
+
+            for feature in sorted(feature_freq.keys()):
+                vector[feature] = feature_freq[feature] * self.idf[feature]
+
+            vectors.append((text, vector))
+
         return vectors
 
-    def classify_aggression(self,texts):
+    def classify_aggression(self, texts):
         outcome = []
         instances = self.tweet_2_features(texts)
         for instance in instances:
             classification = self.clf.predict(instance[1])
             proba = self.clf.predict_proba(instance[1])
-            outcome.append((instance[0],classification[0],proba.tolist()[0][-1]))
+            outcome.append((instance[0], classification[0], proba.tolist()[0][-1]))
+
         return outcome
 
-    def main(self,texts):
+    def main(self, texts):
         return self.classify_aggression(texts)
